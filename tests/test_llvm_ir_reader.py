94c4018f0def5c1065181209bbc1fd25b5e6faa9
Jack Styles <jack.styles@arm.com>
2024-11-21 14:21:30 +0000

    [black] Add support for Black in tests
    This extends the code formatting support into the test directory, with
    the CI runs being completed across the whole repository rather than just
    the `src/` directory.

--- tests/test_llvm_ir_reader.py
+++ tests/test_llvm_ir_reader.py
@@ -1,7 +1,9 @@
 import unittest
 import sys
+
 sys.path.append("src/")
 from llvm_ir_reader import extract_instructions
+
 
 class TestStringMethods(unittest.TestCase):
     def test_extract_instructions_with_valid_vhcadd_instructions(self):
@@ -9,14 +11,17 @@ class TestStringMethods(unittest.TestCase):
         instructions = extract_instructions(file)
         file.close()
 
-        self.assertEqual(instructions, ["vhcadd.s32 q2, q1, q0, #270", "vhcadd.s8 q2, q1, q0, #90"])
-    
+        self.assertEqual(
+            instructions, ["vhcadd.s32 q2, q1, q0, #270", "vhcadd.s8 q2, q1, q0, #90"]
+        )
+
     def test_extract_instruction_without_supported_instructions(self):
         file = open("tests/mocks/fake_test_no_supported_instructions.ll")
         instructions = extract_instructions(file)
         file.close()
 
         self.assertEqual(instructions, [])
+
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_validate_instruction.py
+++ tests/test_validate_instruction.py
@@ -2,7 +2,12 @@ import unittest
 import sys
 
 sys.path.append("src/")
-from validate_instruction import validate_instruction, VhcaddInstruction, InstructionValidity
+from validate_instruction import (
+    validate_instruction,
+    VhcaddInstruction,
+    InstructionValidity,
+)
+
 
 class TestStringMethods(unittest.TestCase):
     def test_validate_instruction_with_valid_earlyclobber_instruction(self):
@@ -11,54 +16,54 @@ class TestStringMethods(unittest.TestCase):
 
         self.assertTrue(result.get_result())
         self.assertFalse(result.get_is_regex())
-    
+
     def test_validate_instruction_with_invalid_earlyclobber_instruction(self):
         inst = "vhcadd.s32 q0, q0, q0, #270"
         result = validate_instruction(inst)
 
         self.assertFalse(result.get_result())
         self.assertFalse(result.get_is_regex())
-    
+
     def test_validate_instruction_with_valid_instruction(self):
         inst = "vhcadd.s8 q2, q1, q0, #90"
         result = validate_instruction(inst)
 
         self.assertTrue(result.get_result())
         self.assertFalse(result.get_is_regex())
-    
+
     def test_validate_instruction_with_regex_instruction(self):
         inst = r"vhcadd.s8 q{{[0-9]+}}, q{{[0-9]+}}, q{{[0-9]+}}, #90"
         result = validate_instruction(inst)
 
         self.assertFalse(result.get_result())
         self.assertTrue(result.get_is_regex())
-    
+
     def test_VhcaddInstruction_is_earlyclobber_with_earlyclobber_instruction(self):
         inst = VhcaddInstruction("vhcadd.s32 q2, q1, q0, #270")
 
         self.assertTrue(inst.is_earlyclobber())
-    
+
     def test_VhcaddInstruction_is_earlyclobber_with_non_earlyclobber_instruction(self):
         inst = VhcaddInstruction("vhcadd.s8 q2, q1, q0, #270")
 
         self.assertFalse(inst.is_earlyclobber())
-    
+
     def test_InstructionValidity_get_result_with_false_result(self):
         result = InstructionValidity(False, False)
         result._result = False
 
         self.assertFalse(result.get_result())
-    
+
     def test_InstructionValidity_get_result_with_true_result(self):
         result = InstructionValidity(False, False)
         result._result = True
-    
+
     def test_InstructionValidity_get_result_with_false_is_regex(self):
         result = InstructionValidity(False, False)
         result._is_regex = False
 
         self.assertFalse(result.get_is_regex())
-    
+
     def test_InstructionValidity_get_result_with_true_is_regex(self):
         result = InstructionValidity(False, False)
         result._is_regex = True
