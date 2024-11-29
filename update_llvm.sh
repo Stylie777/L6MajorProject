291b43a021a5291574d2b576618c26ede1dfb1b4
Jack Styles <jack.styles@arm.com>
2024-11-29 14:56:29 +0000

    [ci] Update LLVM update script
    This updates the script to better utilise the options available
    through git for updating the submodule.

--- update_llvm.sh
+++ update_llvm.sh
@@ -3,9 +3,7 @@ git config --global user.name "GitHub Action"
 git config --global user.email "noreply@github.com"
 
 # Update llvm
-cd llvm-project/
-git pull origin main
-cd ../
+git submodule update --remote --merge
 git add llvm-project
 git commit -m "[llvm-project] Update llvm-project version"
 git push origin main
