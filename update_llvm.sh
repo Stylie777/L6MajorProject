9272cbff28c70edc60901454b32aa70d16c99a0e
Jack Styles <jack.styles@arm.com>
2024-11-30 12:01:18 +0000

    [update-llvm] Remove --merge option
    This was causing issues in CI, so the option is removed to make
    the CI more stable.

--- update_llvm.sh
+++ update_llvm.sh
@@ -3,7 +3,7 @@ git config --global user.name "GitHub Action"
 git config --global user.email "noreply@github.com"
 
 # Update llvm
-git submodule update --remote --merge
+git submodule update --remote
 git add llvm-project
 git commit -m "[llvm-project] Update llvm-project version"
 git push origin main
